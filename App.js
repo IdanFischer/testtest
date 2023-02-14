0b440d5a05ce8a6914d0991a312d92917a8fc2a2
Idan Fischer <Idanfischer5@gmail.com>
2023-02-14 17:29:44 -0500

    tested git cloning

---
+++ App.js
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import Feed from "./src/components/Feed";
+import Details from "./src/components/Details";
+
+const Stack = createNativeStackNavigator();
+export const PostContext = createContext();
+
+export default function App() {
+  const [currentPost, setCurrentPost] = useState();
+  return (
+    <PostContext.Provider value={{ currentPost, setCurrentPost }}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Feed} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PostContext.Provider>
+  );
+}
